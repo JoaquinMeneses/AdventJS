4a07f8c3651b352f861675cb38089a49c97ed8ca
Joaquin Meneses <joakinmeneses.jm@gmail.com>
2023-12-06 08:08:45 -0300

    Add:solution to challenge 1

---
+++ src/challenges/challenge1.js
@@ -0,0 +1,16 @@
+function findFirstRepeated(gifts) {
+  const giftRepeated = gifts.find((gift, i) => gifts.indexOf(gift) !== i);
+  return giftRepeated ?? -1;
+}
+
+const giftIds = [2, 1, 3, 5, 3, 2];
+const firstRepeatedId = findFirstRepeated(giftIds);
+console.log(firstRepeatedId);
+
+const giftIds2 = [1, 2, 3, 4];
+const firstRepeatedId2 = findFirstRepeated(giftIds2);
+console.log(firstRepeatedId2);
+
+const giftIds3 = [5, 1, 5, 1];
+const firstRepeatedId3 = findFirstRepeated(giftIds3);
+console.log(firstRepeatedId3);
